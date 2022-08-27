2b7d50006cde6f76b4b73ebe6673486f47e60d67
OleksiiChernii <oleksii.chernii@gmail.com>
2022-08-27 19:37:06 +0300

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+ingredients.forEach(element => {
+  let listItem = document.createElement("li");
+  listItem.innerHTML = element;
+  listItem.classList.add("item");
+  
+  document.querySelector("#ingredients").append(listItem);
+})

774cfe2b31d06dbc1783ae3a854d41227028c632
OleksiiChernii <oleksii.chernii@gmail.com>
2022-08-30 21:08:49 +0300

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,13 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+      } = event.currentTarget;
+
+    if (email.value === "" && password.value === "") {
+        alert("Please fill in all the fields!");
+    }
+    event.currentTarget.reset();
+})

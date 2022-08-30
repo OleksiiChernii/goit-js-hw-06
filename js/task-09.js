b6114c3d28197e0e520ba193a84b90d6d3132612
OleksiiChernii <oleksii.chernii@gmail.com>
2022-08-30 21:15:25 +0300

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonColor = document.querySelector('.change-color');
+buttonColor.addEventListener('click', () => {
+  let randomColor = getRandomHexColor();
+  document.querySelector('.color').textContent = randomColor;
+  document.body.style.backgroundColor = randomColor;
+})

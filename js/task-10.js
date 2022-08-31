e8b1d1c719ef674ff2e92c272f3e1fc546b08a8b
OleksiiChernii <oleksii.chernii@gmail.com>
2022-08-31 15:30:30 +0300

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,27 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const boxes = document.querySelector('#boxes');
+
+document.querySelector('[data-create]').addEventListener('click', ()=>{
+  createBoxes(document.querySelector('input').value);
+})
+
+document.querySelector('[data-destroy]').addEventListener('click', destroyBoxes);
+
+function createBoxes(amount){
+  destroyBoxes();
+  for(let i = 0; i < amount; i += 1){
+    let box = document.createElement('div');
+    box.style.width = 30 + i * 10 + 'px';
+    box.style.height = '30px';
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.insertAdjacentElement('beforeend', box);
+  }
+}
+
+function destroyBoxes(){
+  boxes.innerHTML = "";
+}
